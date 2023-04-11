3b8440491960110cd38a5630cf66a58895e358c0
saschaalda <sascha.alda@h-brs.de>
2023-04-11 19:12:11 +0200

    Neuer Kommentar in Builde

--- src/org/hbrs/se2/registration/test/TestRegistrationControl.java
+++ src/org/hbrs/se2/registration/test/TestRegistrationControl.java
@@ -24,7 +24,6 @@ public class TestRegistrationControl {
 	
 	@Test
 	public void testWithFactory() {
-		// Sehr lange Methoden
 		UserDTO dto = UserFactory.createDefaultUserWithNoPassword();
 		RegistrationResult result = regc.registerUser( dto );
 		
@@ -32,17 +31,21 @@ public class TestRegistrationControl {
 		assertEquals(  RegistrationResult.PASSWORD_MISSING, result.getReason() );
 
 		// Diskussion Methoden:
-        // createDefaultUser -  WithNoPassword - And - NoAddress- And - WithNOEmail
+        // createDefaultUser WithNoPassword And NoAddress And WithNOEmail
 
 	}
 
 	@Test
 	public void testDiverseUsers() {
 		// Test Builder Pattern:
-		UserDTO dto = UserBuilder.please().createDefaultUser().withNoAddress().done();
-		dto = UserBuilder.please().createDefaultUser().withNoPassword().and().withUserID("abc").thatsall();
-		dto = UserBuilder.please().createDefaultUser().withNoPassword().but().withGebDatum("1.1.2000").done();
-		dto = UserBuilder.please().createDefaultUser().withUserID("2").and().withGebDatum("dndn").but().withNoPassword().thatsall();
+		UserDTO dto = null;
+
+		UserBuilder.please()
+				.createDefaultUser()
+				.withNoAddress()
+				.thatsall();
+		UserBuilder.please().createDefaultUser().withNoPassword().and().withUserName("alda").done();
+		UserBuilder.please().createDefaultUser().withNoPassword().but().withAddress("Bonn").thatsall();
 
 		RegistrationResult result = regc.registerUser( dto );
 
@@ -72,7 +75,7 @@ public class TestRegistrationControl {
 		System.out.println( dto );
 		assertEquals( dto , dto2 );
 		
-		// Optionales Feld auslassen
+		// Optionales Feld auslassen!
 		dto2 = UserBuilder.please().createDefaultUser().but().withNoAddress().thatsall();
 		result = this.regc.registerUser(dto2);
 		assertTrue( result.getResult() );
